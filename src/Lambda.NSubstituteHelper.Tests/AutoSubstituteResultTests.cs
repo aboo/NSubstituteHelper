adfdb890a21e43a58c8caa39011bc3af00e9c1e9
Aboo <azarnoush@gmail.com>
2018-02-03 00:06:36 +1100

    Implemented Auto Substitute Result
    - added tests and implementation

--- src/Lambda.NSubstituteHelper.Tests/AutoSubstituteResultTests.cs
+++ src/Lambda.NSubstituteHelper.Tests/AutoSubstituteResultTests.cs
@@ -1,10 +1,67 @@
-﻿using Xunit;
+﻿using System.Collections.Generic;
+using NFluent;
+using Xunit;
+using NSubstitute;
 
 namespace Lambda.NSubstituteHelper.Tests
 {
 	public class AutoSubstituteResultTests
 	{
 		[Fact]
-		public void 
+		public void Properties_Check()
+		{
+			// arrange & act
+			const string key = "key";
+			var value = new object();
+			var target = new object();
+
+			var substitutes = new Dictionary<string, object>
+			{
+				{key, value}
+			};
+
+			var model = new AutoSubstitutedResult<object>
+			{
+				Substitutes = substitutes,
+				Target = target
+			};
+
+			// assert
+			Check.That(model).HasFieldsWithSameValues(new
+			{
+				Substitutes = substitutes,
+				Target = target
+			});
+
+			Check.That(model.Target).Equals(target);
+			Check.That(model.Substitutes).Equals(substitutes);
+		}
+
+		[Fact]
+		public void Substitutes_Instantiates_ByDefault()
+		{
+			// arrange & act
+			var model = new AutoSubstitutedResult<object>();
+
+			// assert
+			Check.That(model.Substitutes).IsNotNull();
+		}
+
+		[Fact]
+		public void Get_ReturnsTheInstance_WhenASubstituteInstanceExists()
+		{
+			// arrange
+			var model = new AutoSubstitutedResult<object>();
+			var expected = Substitute.For<ITestService>();
+			model.Substitutes.Add(typeof(ITestService).ToString(), expected);
+
+			// act
+			var actual = model.Get<ITestService>();
+
+			// assert
+			Check.That(actual).Equals(expected);
+		}
+
+		public interface ITestService { }
 	}
 }
--- src/Lambda.NSubstituteHelper/AutoSubstitutedResult.cs
+++ src/Lambda.NSubstituteHelper/AutoSubstitutedResult.cs
@@ -1,6 +1,33 @@
-﻿namespace Lambda.NSubstituteHelper
+﻿using System.Collections.Generic;
+
+namespace Lambda.NSubstituteHelper
 {
-	public class AutoSubstitutedResult
+	/// <summary>
+	/// Defines an instance of Auto Substitute Result which holds the result of substitute.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public class AutoSubstitutedResult<T> where T : class
 	{
+		/// <summary>
+		/// Contains all the instances of the substitutes created.
+		/// </summary>
+		public Dictionary<string, object> Substitutes { get; set; } = new Dictionary<string, object>();
+
+		/// <summary>
+		/// Gets or sets the instance of the class with injections.
+		/// </summary>
+		public T Target { get; set; }
+
+		/// <summary>
+		/// Returns an instance of type T2
+		/// </summary>
+		/// <typeparam name="T2">The interface type of the instance</typeparam>
+		/// <returns>The instance</returns>
+		public T2 Get<T2>() where T2 : class
+		{
+			var key = typeof(T2).ToString();
+			var value = Substitutes[key];
+			return value as T2;
+		}
 	}
 }
